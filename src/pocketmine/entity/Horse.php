80ea0d7f9e10ebc4600fdb6527417e87d6cebd02
Tcanw <825814373@qq.com>
2017-07-08 07:30:44 +0800

    These no update

---
+++ src/pocketmine/entity/Horse.php
@@ -0,0 +1,75 @@
+<?php
+
+ /*
+ *  _______                                     ______  _
+ * /  ____ \                                   |  __  \| \
+ * | |    \_|              _                   | |__| || |
+ * | |   ___  ___  _  ___ (_) ___  __    _ ___ |  ____/| | _   _  ___
+ * | |  |_  |/(_)\| '/_  || |/___\(_)\  ///___\| |     | || | | |/___\
+ * | \___|| | |___| |  | || |_\_\   \ \// _\_\ | |     | || | | |_\_\
+ * \______/_|\___/|_|  |_||_|\___/   \ /  \___/|_|     |_||__/,_|\___/
+ *                                   //
+ *                                  (_)                Power by:
+ *                                                           Tesseract
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @由Tessetact团队创建，GenisysPlus项目组修改
+ * @链接 https://github.com/TesseractTeam
+ * @链接 https://github.com/Tcanw/GenisysPlus
+ *
+ */
+
+namespace pocketmine\entity;
+
+use pocketmine\Player;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\protocol\MobArmorEquipmentPacket;
+
+class Horse extends Living{
+
+	const NETWORK_ID = 23;
+
+	public function getName() : string{
+		return "Horse";
+	}
+	
+	public function setChestPlate($id){
+		/*	
+		416, 417, 418, 419 only
+		*/
+		$pk = new MobArmorEquipmentPacket();
+		$pk->eid = $this->getId();
+		$pk->slots = [
+		ItemItem::get(0,0),
+		ItemItem::get($id,0),
+		ItemItem::get(0,0),
+		ItemItem::get(0,0)
+		];
+		foreach($this->level->getPlayers() as $player){
+			$player->dataPacket($pk);
+		}
+	}
+	
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->eid = $this->getId();
+		$pk->type = self::NETWORK_ID;
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = $this->motionX;
+		$pk->speedY = $this->motionY;
+		$pk->speedZ = $this->motionZ;
+		$pk->yaw = $this->yaw;
+		$pk->pitch = $this->pitch;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
+
+		parent::spawnTo($player);
+	}
+
+}
